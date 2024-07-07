8617d65f3b3bfcf8f23c12cc86a7949e6de540ab
nishantvekariya1 <nishantvekariya623@gmail.com>
2024-07-07 16:59:57 +0530

    implement update user profile, update password and mark notification read at server side

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,13 @@
 import express from "express";
 import {
+  changeUserPassword,
   getNotificationsList,
   getTeamList,
   loginUser,
   logoutUser,
+  markNotificationRead,
   registerUser,
+  updateUserProfile,
 } from "../controllers/userController.js";
 import { isAdminRoute, protectRoute } from "../middlewares/authMiddlewaves.js";
 
@@ -16,5 +19,14 @@ router.post("/logout", logoutUser);
 
 router.get("/get-team", protectRoute, isAdminRoute, getTeamList);
 router.get("/notifications", protectRoute, getNotificationsList);
+
+router.put("/profile", protectRoute, updateUserProfile);
+router.put("/read-noti", protectRoute, markNotificationRead);
+router.put("/change-password", protectRoute, changeUserPassword);
+
+// router
+//   .route("/:id")
+//   .put(protectRoute, isAdminRoute, activateUserProfile)
+//   .delete(protectRoute, isAdminRoute, deleteUserProfile);
 
 export default router;
